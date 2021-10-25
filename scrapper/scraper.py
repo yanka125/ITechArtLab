13b0b0557f756b698f58f1fde4fce503d81a126c
yanka125 <yanka7356@gmail.com>
2021-10-25 16:57:53 +0300

    Completely redesigned the parsing algorithm
    Changed the logic of searching for posts and processing data in them.
    Now this program can receive one hundred posts, but without checking
    empty elements.
    
    – need to determine how to parse data from pop-up.
    
    – need to add documentation for methods.
    
    – need to add type annotation
    
    – need to optimize requests to speed up the program
    
    – need to add data validation before adding to the result list (file)
    
    – need to add event logging

--- scrapper/scraper.py
+++ scrapper/scraper.py
@@ -1,76 +1,120 @@
-import time
-from datetime import datetime
+from pprint import pprint
 from uuid import uuid1
+from datetime import datetime
 
+import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
 
+headers = {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"
+}
+
+url = "https://www.reddit.com/top/?t=month"
 now = datetime.now().strftime("%Y%m%d%H%M")
 
-# Web scrapper for infinite scrolling page
-driver = webdriver.Chrome(executable_path=r"D:\chromedriver.exe")
-driver.maximize_window()
-driver.get("https://www.reddit.com/top/?t=month")
-time.sleep(3)  # Allow 3 seconds for the web page to open
-scroll_pause_time = 2  # Time between scrolling
-screen_height = driver.execute_script(
-    "return window.screen.height;")  # get the screen height of the web
-i = 1
 
-while True:
-    # scroll one screen height each time
-    driver.execute_script(
-        "window.scrollTo(0, {screen_height}*{i});".format(screen_height=screen_height, i=i))
-    i += 1
-    time.sleep(scroll_pause_time)
-    # update scroll height after scrolled, as the scroll height can change after scrolling the page
-    scroll_height = driver.execute_script("return document.body.scrollHeight;")
-    # Break the loop when the height we need to scroll to is larger than the total scroll height
-    # if (screen_height) * i > scroll_height:
-    #     break
-    if i > 100:
-        break
+def scrapper():
+    s = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=s)
+    driver.maximize_window()
+    driver.get(url)
+    result_list = []
+    i = 1
 
-# Extract Reddit Data
-urls = []
-unique_ids = []
-dates = []
-user_names = []
-post_categories = []
-numbers_of_comments = []
-numbers_of_vote = []
-soup = BeautifulSoup(driver.page_source, "html.parser")
-for url in soup.find_all("a", class_="_3jOxDPIQ0KaOWpzvSQo-1s"):
-    urls.append(url.get("href"))  # Append post url to list
-    if len(urls) > 100:
-        break
-for i in range(len(urls)):
-    unique_id = uuid1().hex
-    unique_ids.append(unique_id)
+    while True:
+        try:
+            unique_id = uuid1().hex
+            element = driver.find_element(By.XPATH,
+                                          "(//div[@data-testid = 'post-container'])[" + str(
+                                              i) + "]")
+            # actions = ActionChains(driver)
+            # date = element.find_element(By.XPATH,
+            #                                 "(//a[@class='_3jOxDPIQ0KaOWpzvSQo-1s'])[" + str(
+            #                                     i) + "]")
+            # actions.move_to_element(date).perform()
+            # post_date = date.find_element(By.XPATH,
+            #                                        "(//div[@style='position: absolute; inset: auto auto 0px 0px; transform: translate(296px, -535px);'])").text
+            post_category = element.find_element(By.XPATH,
+                                                 "(//div[@class='_2mHuuvyV9doV3zwbZPtIPG']/a[@class='_3ryJoIoycVkA88fy40qNJc'])[" + str(
+                                                     i) + "]").text[2:]
+            number_of_votes = element.find_element(By.XPATH,
+                                                   "(//div[@class='_1rZYMD_4xY3gRcSS3p8ODO _3a2ZHWaih05DgAOtvu6cIo '])[" + str(
+                                                       i) + "]").text
+            number_of_comments = element.find_element(By.XPATH,
+                                                      "(//span[@class='FHCV02u6Cp2zYL0fhQPsO'])[" + str(
+                                                          i) + "]").text
+            post_url = element.find_element(By.XPATH,
+                                            "(//a[@class='_3jOxDPIQ0KaOWpzvSQo-1s'])[" + str(
+                                                i) + "]").get_attribute("href")
+            user_url = element.find_element(By.XPATH,
+                                            "(//div[@class='_2mHuuvyV9doV3zwbZPtIPG']/a[@style='color: rgb(120, 124, 126);'])[" + str(
+                                                i) + "]").get_attribute('href')
+            user_name = user_url[user_url.index('/user/') + 6:len(user_url) - 1]
 
-for item in soup.find_all('div', class_="_3AStxql1mQsrZuUIFP9xSg nU4Je7n-eSXStTBAPMYt8"):
-    data = item.text
-    dates.append(data[:7])  # Append post date to list, need to refactor! [data.index('days') - 2:]
-    user_names.append(data[
-                      :7])  # Append username to list, need to refactor! data.index('u/') + 2:data.index('days') - 2
-    post_categories.append(
-        data[data.index('r/') + 2:data.index('•')])  # Append post category to list
-    if len(dates) > 100 or len(user_names) > 100 or len(post_categories) > 100:
-        break
+            response = requests.get(url=user_url, headers=headers)
+            soup = BeautifulSoup(response.text, "lxml")
+            elem = soup.find("script", {"id": "data"}).text
+            index_carma = elem.index('"karma":{"fromAwards')
+            all_carma = elem[index_carma:index_carma + 120]
+            try:
+                post_karma = all_carma[
+                             all_carma.index('"fromPosts":') + 12:all_carma.index('"total"') - 1]
+            except Exception as _ex:
+                post_karma = None
+            try:
+                comment_karma = all_carma[all_carma.index('"fromComments":') + 15:all_carma.index(
+                    '"fromPosts"') - 1]
+            except Exception as _ex:
+                comment_karma = None
+            try:
+                user_karma = soup.find("span", {
+                    "id": "profile--id-card--highlight-tooltip--karma"}).text
+            except Exception as _ex:
+                user_karma = None
+            try:
+                user_cake_day = soup.find("span", {
+                    "id": "profile--id-card--highlight-tooltip--cakeday"}).text
+            except Exception as _ex:
+                user_cake_day = None
 
-for number in soup.find_all('span', class_="FHCV02u6Cp2zYL0fhQPsO"):
-    numbers_of_comments.append(number.text)
-    if len(numbers_of_comments) > 100:
-        break
+            actions = ActionChains(driver)
+            actions.move_to_element(element).perform()
+            i += 1
+            # if unique_id or post_url or user_name or number_of_comments or number_of_votes \
+            #         or post_category or user_carma or user_cake_day == 'None':
+            #     continue
 
-for number in soup.find_all('div', class_="_1rZYMD_4xY3gRcSS3p8ODO _3a2ZHWaih05DgAOtvu6cIo"):
-    numbers_of_vote.append(number.text)
-    if len(numbers_of_vote) > 100:
-        break
+            result_list.append(
+                {
+                    "unique_id": unique_id,
+                    "post_url": post_url,
+                    "user_name": user_name,
+                    "user_karma": user_karma,
+                    "user_cake_day": user_cake_day,
+                    "post_karma": post_karma,
+                    "comment_karma": comment_karma,
+                    # "post_date": post_date,
+                    "number_of_comments": number_of_comments,
+                    "number_of_votes": number_of_votes,
+                    "post_category": post_category,
+                }
+            )
 
-for i in range(len(urls)):
-    str = urls[i] + ';' + unique_ids[i] + ';' + dates[i] + ';' + post_categories[i] + ';' + \
-          numbers_of_comments[i] + ';' + numbers_of_vote[i] + ';' + '\n'
+            if len(result_list) == 100:
+                # pprint(result_list)
+                with open("reddit-" + now + ".txt", 'w') as file:
+                    for i in range(len(result_list)):
+                        file.write(str(result_list[i]) + '\n')
+                break
+        except Exception as e:
+            print(e)
+            break
 
-    with open("reddit-" + now + ".txt", 'a') as file:
-        file.write(str)
+
+scrapper()
